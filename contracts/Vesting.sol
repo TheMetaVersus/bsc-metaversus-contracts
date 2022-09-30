5502f99c466232d81252a7953ac37519319f0427
Nguyễn Lê Anh Minh <minh.nguyen@napaglobal.com>
2022-09-30 19:52:53 +0700

    Add validation to vesting contract

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -1,134 +1,132 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity 0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
 
-// import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-// import "./Adminable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
-// /**
-//  *  @title  Dev Vesting Contract
-//  *
-//  *  @author Metaversus Team
-//  *
-//  *  @notice Init info about amount of token percentage to users at initial,
-//  *          user must claim by themselves
-//  */
-// contract Vesting is Initializable, Adminable {
-//     using SafeERC20Upgradeable for IERC20Upgradeable;
+import "./Validatable.sol";
 
-//     IERC20Upgradeable public token;
+/**
+ *  @title  Dev Vesting Contract
+ *
+ *  @author Metaversus Team
+ *
+ *  @notice Init info about amount of token percentage to users at initial,
+ *          user must claim by themselves
+ */
+contract Vesting is Validatable {
+    using SafeERC20Upgradeable for IERC20Upgradeable;
 
-//     struct Vest {
-//         address owner;
-//         uint256 amount;
-//         uint256 stakeType;
-//         uint256 start;
-//         uint256 initial;
-//         uint256 cliff;
-//         uint256 linear;
-//         uint256 claimed;
-//     }
+    IERC20Upgradeable public token;
 
-//     mapping(bytes32 => Vest) private _vests;
-//     mapping(address => uint256) private _nonce;
+    struct Vest {
+        address owner;
+        uint256 amount;
+        uint256 stakeType;
+        uint256 start;
+        uint256 initial;
+        uint256 cliff;
+        uint256 linear;
+        uint256 claimed;
+    }
 
-//     event InitiateVests(
-//         address[] indexed accounts,
-//         uint256[] amounts,
-//         uint256[] initials,
-//         uint256 stakeType,
-//         uint256 indexed _cliff,
-//         uint256 indexed _linear
-//     );
-//     event Claim(address indexed account, uint256 indexed tokenClaimable);
+    mapping(bytes32 => Vest) private _vests;
+    mapping(address => uint256) private _nonce;
 
-//     function initialize(address owner_, IERC20Upgradeable _token) public initializer {
-//         Adminable.__Adminable_init();
-//         _transferOwnership(owner_);
-//         token = _token;
-//     }
+    event InitiateVests(
+        address[] indexed accounts,
+        uint256[] amounts,
+        uint256[] initials,
+        uint256 stakeType,
+        uint256 indexed _cliff,
+        uint256 indexed _linear
+    );
+    event Claim(address indexed account, uint256 indexed tokenClaimable);
 
-//     function claim(address owner_, uint256 nonce) external {
-//         uint256 tokenClaimable = getClaimable(owner_, nonce);
-//         require(tokenClaimable > 0, "Vesting: No token to claim");
-//         bytes32 index = getVestId(owner_, nonce);
-//         _vests[index].claimed = _vests[index].claimed + tokenClaimable;
-//         token.safeTransfer(owner_, tokenClaimable);
+    function initialize(IERC20Upgradeable _token, IAdmin _admin) public initializer {
+        __Validatable_init(_admin);
+        token = _token;
+    }
 
-//         emit Claim(owner_, tokenClaimable);
-//     }
+    function claim(address owner_, uint256 nonce) external {
+        uint256 tokenClaimable = getClaimable(owner_, nonce);
+        require(tokenClaimable > 0, "Vesting: No token to claim");
+        bytes32 index = getVestId(owner_, nonce);
+        _vests[index].claimed = _vests[index].claimed + tokenClaimable;
+        token.safeTransfer(owner_, tokenClaimable);
 
-//     function initiateVests(
-//         address[] memory accounts,
-//         uint256[] memory amounts,
-//         uint256[] memory initials,
-//         uint256 stakeType,
-//         uint256 totalAmount,
-//         uint256 cliff,
-//         uint256 linear
-//     ) external onlyOwnerOrAdmin {
-//         require(accounts.length > 0, "Vesting: Bad length");
-//         require(accounts.length == amounts.length && amounts.length == initials.length, "Vesting: Mismatched inputs");
+        emit Claim(owner_, tokenClaimable);
+    }
 
-//         uint256 amount = 0;
-//         for (uint256 i = 0; i < accounts.length; i++) {
-//             amount += amounts[i];
-//             initiateVest(accounts[i], amounts[i], stakeType, initials[i], cliff, linear);
-//         }
+    function initiateVests(
+        address[] memory accounts,
+        uint256[] memory amounts,
+        uint256[] memory initials,
+        uint256 stakeType,
+        uint256 totalAmount,
+        uint256 cliff,
+        uint256 linear
+    ) external onlyAdmin {
+        require(accounts.length > 0, "Vesting: Bad length");
+        require(accounts.length == amounts.length && amounts.length == initials.length, "Vesting: Mismatched inputs");
 
-//         require(amount == totalAmount, "Vesting: Bad totalAmount");
-//         token.safeTransferFrom(_msgSender(), address(this), totalAmount);
+        uint256 amount = 0;
+        for (uint256 i = 0; i < accounts.length; i++) {
+            amount += amounts[i];
+            initiateVest(accounts[i], amounts[i], stakeType, initials[i], cliff, linear);
+        }
 
-//         emit InitiateVests(accounts, amounts, initials, stakeType, cliff, linear);
-//     }
+        require(amount == totalAmount, "Vesting: Bad totalAmount");
+        token.safeTransferFrom(_msgSender(), address(this), totalAmount);
 
-//     function initiateVest(
-//         address owner_,
-//         uint256 amount,
-//         uint256 stakeType,
-//         uint256 initial,
-//         uint256 cliff,
-//         uint256 linear
-//     ) public onlyOwnerOrAdmin returns (bytes32) {
-//         require(initial < amount, "Vesting: initial amount should be less than total amount.");
-//         bytes32 index = getVestId(owner_, _nonce[owner_]);
-//         _vests[index] = Vest(owner_, amount, stakeType, block.timestamp, initial, cliff, linear, 0);
-//         _nonce[owner_]++;
-//         return index;
-//     }
+        emit InitiateVests(accounts, amounts, initials, stakeType, cliff, linear);
+    }
 
-//     function getVestId(address user, uint256 nonce) public pure returns (bytes32) {
-//         return keccak256(abi.encodePacked(user, nonce));
-//     }
+    function initiateVest(
+        address owner_,
+        uint256 amount,
+        uint256 stakeType,
+        uint256 initial,
+        uint256 cliff,
+        uint256 linear
+    ) public onlyAdmin returns (bytes32) {
+        require(initial < amount, "Vesting: initial amount should be less than total amount.");
+        bytes32 index = getVestId(owner_, _nonce[owner_]);
+        _vests[index] = Vest(owner_, amount, stakeType, block.timestamp, initial, cliff, linear, 0);
+        _nonce[owner_]++;
+        return index;
+    }
 
-//     function getNonce(address user) public view returns (uint256) {
-//         return _nonce[user];
-//     }
+    function getVestId(address user, uint256 nonce) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(user, nonce));
+    }
 
-//     function getVest(bytes32 index) public view returns (Vest memory) {
-//         return _vests[index];
-//     }
+    function getNonce(address user) public view returns (uint256) {
+        return _nonce[user];
+    }
 
-//     function getVestType(bytes32 index) public view returns (uint256) {
-//         return _vests[index].stakeType;
-//     }
+    function getVest(bytes32 index) public view returns (Vest memory) {
+        return _vests[index];
+    }
 
-//     function getClaimable(address owner_, uint256 nonce) public view returns (uint256) {
-//         bytes32 index = getVestId(owner_, nonce);
-//         uint256 tokenClaimable;
-//         if (
-//             block.timestamp >= _vests[index].start + _vests[index].cliff &&
-//             block.timestamp <= _vests[index].start + _vests[index].cliff + _vests[index].linear
-//         ) {
-//             uint256 timePassed = block.timestamp - (_vests[index].start + _vests[index].cliff);
-//             tokenClaimable =
-//                 (((_vests[index].amount - _vests[index].initial) * timePassed) / _vests[index].linear) +
-//                 _vests[index].initial -
-//                 _vests[index].claimed;
-//         } else if (block.timestamp > _vests[index].start + _vests[index].cliff + _vests[index].linear) {
-//             tokenClaimable = _vests[index].amount - _vests[index].claimed;
-//         }
-//         return tokenClaimable;
-//     }
-// }
+    function getVestType(bytes32 index) public view returns (uint256) {
+        return _vests[index].stakeType;
+    }
+
+    function getClaimable(address owner_, uint256 nonce) public view returns (uint256) {
+        bytes32 index = getVestId(owner_, nonce);
+        uint256 tokenClaimable;
+        if (
+            block.timestamp >= _vests[index].start + _vests[index].cliff &&
+            block.timestamp <= _vests[index].start + _vests[index].cliff + _vests[index].linear
+        ) {
+            uint256 timePassed = block.timestamp - (_vests[index].start + _vests[index].cliff);
+            tokenClaimable =
+                (((_vests[index].amount - _vests[index].initial) * timePassed) / _vests[index].linear) +
+                _vests[index].initial -
+                _vests[index].claimed;
+        } else if (block.timestamp > _vests[index].start + _vests[index].cliff + _vests[index].linear) {
+            tokenClaimable = _vests[index].amount - _vests[index].claimed;
+        }
+        return tokenClaimable;
+    }
+}
